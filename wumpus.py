afe5dea9c34e10bdf3fa44b27226b97b83163c0f
Nathan Hall <nlh257@live.missouristate.edu>
2017-07-13 20:52:36 -0500

    Check diffs

--- wumpus.py
+++ wumpus.py
@@ -1,6 +1,15 @@
 from random import choice
 
-cave_numbers = range(1,21)
+cave_numbers = range(0,20)
+caves = []
+for i in cave_numbers:
+    caves.append([])
+
+for i in cave_numbers:
+    for j in range(3):
+        passage_to = choice(cave_numbers)
+        caves[i].append(passage_to)
+
 wumpus_location = choice(cave_numbers)
 wumpus_friend_location = choice(cave_numbers)
 player_location = choice(cave_numbers)
@@ -10,23 +19,24 @@ while (player_location == wumpus_location or
 
 print ("Welcome to Hunt the Wumpus!")
 print ("You can see", len(cave_numbers), "caves")
-print("To play, just type the number")
-print("of the cave you wish to enter next")
+print ("To play, just type the number")
+print ("of the cave you wish to enter next")
 
 while True:
-    print("You are in cave", player_location)
-    if (player_location == wumpus_location - 1 or
-        player_location == wumpus_location + 1):
-        print("I smell a wumpus!")
-    if (player_location == wumpus_friend_location - 1 or
-        player_location == wumpus_friend_location + 1):
-        print("I smell an even stinkier wumpus!")
+    print ("You are in cave", player_location)
+    print ("From here, you can see caves:", caves[player_location])
+    if wumpus_location in caves[player_location]:
+        print ("I smell a wumpus!")
+    if wumpus_friend_location in caves[player_location]:
+        print ("I smell an even stinkier wumpus!")
     
     print("Which cave next?")
     player_input = input(">")
     if (not player_input.isdigit() or
-        int(player_input) not in cave_numbers):
-        print(player_input, "is not a cave!")
+        int(player_input) not in caves[player_location]):
+        print (player_input + "?")
+        print ("That's not a direction that I can see!")
+        continue
     
     else:
         player_location = int(player_input)
